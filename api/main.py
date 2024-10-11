ae4532009a52829190889c3e9b35b754ba7bfc6f
villoslado <jg.villoslado@gmail.com>
2024-10-10 21:51:13 -0700

    Added Dockerfiles, docker compose, and enhanced mainpy

--- api/main.py
+++ api/main.py
@@ -1,7 +1,17 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.environ.get("CORS_HOST", "http://localhost:5173")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -12,3 +22,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
