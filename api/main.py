a23874bb2cd388147e59b6e0ba47038e2d52bba6
villoslado <jg.villoslado@gmail.com>
2024-10-23 15:05:06 -0700

    Edited main.py to include routers

--- api/main.py
+++ api/main.py
@@ -2,7 +2,19 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# from routers import () - to be added when we build routers
+from routers.account_router import router as account_router
+from routers.activity_router import router as activity_router
+from routers.activity_type_router import router as activity_type_router
+from routers.activity_user_router import router as activity_user_router
+from routers.activity_contact_router import router as activity_contact_router
+from routers.contact_router import router as contact_router
+from routers.country_router import router as country_router
+from routers.forecast_category_router import router as forecast_category_router
+from routers.opportunity_router import router as opportunity_router
+from routers.opportunity_contact_router import router as opportunity_contact_router
+from routers.opportunity_owner_router import router as opportunity_owner_router
+from routers.stage_router import router as stage_router
+from routers.user_router import router as user_router
 
 import os
 
@@ -23,6 +35,20 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(account_router)
+app.include_router(activity_router)
+app.include_router(activity_type_router)
+app.include_router(activity_user_router)
+app.include_router(activity_contact_router)
+app.include_router(contact_router)
+app.include_router(country_router)
+app.include_router(forecast_category_router)
+app.include_router(opportunity_router)
+app.include_router(opportunity_contact_router)
+app.include_router(opportunity_owner_router)
+app.include_router(stage_router)
+app.include_router(user_router)
 
 
 @app.get("/")
